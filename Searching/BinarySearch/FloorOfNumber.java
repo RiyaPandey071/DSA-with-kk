e7dc1bc39cd1ccb2d928b806ed32d7646806ab77
Riya Pandey <riyapandey0553@gmail.com>
2025-01-03 21:33:15 +0530

    new files

---
+++ Searching/BinarySearch/FloorOfNumber.java
@@ -0,0 +1,32 @@
+public class FloorOfNumber {
+    static int Floor(int [] arr,int target){
+
+        int start=0;
+        int end=arr.length -1;
+
+        
+        while(start<=end){
+            int mid=start + (end-start)/2;
+            
+            if(arr[mid]>target){
+                end=mid-1;
+            }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+
+           
+        }
+        return end;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={10,11,12,14,15,17,18};
+        int target=9;
+        System.out.println(Floor(arr,target));
+    }
+
+}
