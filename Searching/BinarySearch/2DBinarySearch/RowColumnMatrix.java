f430d15df8803253eea474915554270cb4e7be21
Riya Pandey <riyapandey0553@gmail.com>
2025-01-06 19:11:28 +0530

    changes in file

--- Searching/BinarySearch/2DBinarySearch/RowColumnMatrix.java
+++ Searching/BinarySearch/2DBinarySearch/RowColumnMatrix.java
@@ -1,20 +1,22 @@
 import java.util.Arrays;
 public class RowColumnMatrix {
   public static void main(String[] args) {
-    int [][] arr={{10,20,30,40},
-{15,25,35,45},
-{28,29,37,49},
-{33,34,38,50}};
+    int [][] arr={{10,20,30,40},{15,25,35,45},
+    {28,29,37,49},
+    {33,34,38,50}
+};
 
-System.out.println(Arrays.toString(search(arr,39  )));
+System.out.println(Arrays.toString(search(arr,20  )));
   } 
    static int[] search(int[][] matrix, int target){
     int row=0;
-    int col=matrix.length-1;
+    int col=matrix[0].length-1;//it should not be matrix.length-1
     while(row<matrix.length && col>=0){
+
         if(matrix[row][col]==target){
             return new int[]{row,col}; 
         }
+
         if(matrix[row][col]<target){
             row++;
         }
@@ -25,3 +27,4 @@ System.out.println(Arrays.toString(search(arr,39  )));
     return new int[]{-1,-1};
    }
 }
+          
