f3ad1b0d449e0877780ad1aad7fc97ea2f730592
Riya Pandey <riyapandey0553@gmail.com>
2025-01-10 21:21:06 +0530

    new file

--- Sorting/bubble.java
+++ Sorting/bubble.java
@@ -13,7 +13,7 @@ public class bubble {
        for(int j=1;j<arr.length-i;j++){
         if(arr[j]<arr[j-1]){
             int temp=arr[j];
-            arr[j]=arr[j-1];
+           arr[j]=arr[j-1];
             arr[j-1]=temp;
             swapped=true;
         }
@@ -24,3 +24,5 @@ public class bubble {
     }
    } 
 }
+
+
