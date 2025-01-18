99e9d31c8b63da6c8510bac872b040dd81863260
Riya Pandey <riyapandey0553@gmail.com>
2025-01-19 02:19:40 +0530

    string

--- STRING/PrettyPrinting.java
+++ STRING/PrettyPrinting.java
@@ -1,5 +1,13 @@
-package STRING;
+
 
 public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a=453.1234f;//% shows placeholder
+        System.out.printf(" Formatted number is %.2f \n",a);
+        //System.out.println(Math.PI);
+        System.out.printf("Pie: %.3f \n",Math.PI);
     
+    System.out.printf("Hello my name is %s and I am %s ","Riya","cool");
+   
+    }
 }
