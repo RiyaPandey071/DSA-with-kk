b17a7df46098cb13dcc2d1cc17e66f1a1186742e
Riya Pandey <riyapandey0553@gmail.com>
2025-01-19 02:47:34 +0530

    string

---
+++ STRING/Method.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Method {
+    public static void main(String[] args) {
+        String name="Riya Pandey";
+        System.out.println(Arrays.toString(name.toCharArray()));
+System.out.println(name.toLowerCase());
+System.out.println(name);
+System.out.println(name.indexOf('a'));
+System.out.println(" Riya ".strip());
+System.out.println(Arrays.toString(name.split(" ")));
+    }
+    
+}
