0b1abb7d2eaa63a2f5fb2074c3510b5fd648b284
Riya Pandey <riyapandey0553@gmail.com>
2025-01-19 02:39:40 +0530

    string

--- STRING/Performance.java
+++ STRING/Performance.java
@@ -1,10 +1,10 @@
 public class Performance {
    public static void main(String[] args) {
     String series="";
-    for(int i=0;i<26;i++){
-        char ch=(char)('a'+i);
-        series=series + ch;
-    }
+   for(int i=0;i<26;i++){
+
+   char ch=(char)('a'+i);
+   series+=ch;}
     System.out.println(series);
    } 
 }
