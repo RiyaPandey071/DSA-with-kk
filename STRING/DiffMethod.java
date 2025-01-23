cb0303c7de5e431c5628caf3edf6c266803ac6da
Riya Pandey <riyapandey0553@gmail.com>
2025-01-23 23:59:56 +0530

    ques

---
+++ STRING/DiffMethod.java
@@ -0,0 +1,11 @@
+public class DiffMethod {
+    public static void main(String[] args) {
+        String txt="My name is \"Riya\"";
+        System.out.println(txt);
+        String txt2="My name is \'Riya\'";
+        System.out.println(txt2);
+        String txt3="My name is \\Riya\\";
+        System.out.println(txt3);
+    }
+    
+}
