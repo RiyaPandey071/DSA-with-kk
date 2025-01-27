3fbabd2f2ece65ae0e81cafa0d7d61d5d3d17689
Riya Pandey <riyapandey0553@gmail.com>
2025-01-27 08:49:45 +0530

    pattern

---
+++ PATTERN/Pattern1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Pattern1 {
+    public static void main(String[] args) {
+       Scanner in=new Scanner(System.in);
+       int n=in.nextInt();
+       pattern(n);
+    }
+       static void pattern(int n){
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }}
+    }
+
