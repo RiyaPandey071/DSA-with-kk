b2cf9fea514ed4bb2ae133d884fd8c3ebf27ee32
Riya Pandey <riyapandey0553@gmail.com>
2025-01-28 11:06:45 +0530

    fibbo

---
+++ RECURSION/Fibbonacci.java
@@ -0,0 +1,17 @@
+
+
+public class Fibbonacci {
+    public static void main(String[] args) {
+      int n = 7;
+      for (int i = 0; i <= n; i++) {
+          System.out.print(fibo(i) + " ");
+      }
+       
+       }
+    static int fibo(int n){
+        if(n<2){
+           return n;
+        }  
+            return fibo(n-1) + fibo(n-2);
+ }
+}
---
+++ RECURSION/Without Recursion/Fibonacci.java
@@ -0,0 +1,24 @@
+public class Fibonacci {
+    
+        public static void main(String[] args) {
+            int n = 10; // number of terms
+            int[] fibSequence = new int[n];
+            
+            if (n > 0) {
+                fibSequence[0] = 0;
+            }
+            if (n > 1) {
+                fibSequence[1] = 1;
+            }
+            
+            for (int i = 2; i < n; i++) {
+                fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
+            }
+            
+            // Print the Fibonacci sequence
+            for (int i = 0; i < n; i++) {
+                System.out.print(fibSequence[i] + " ");
+            }
+        }
+    }
+     
