977fa40dfc03538035632b98ba200ed6e7baeee5
Riya Pandey <riyapandey0553@gmail.com>
2025-01-31 09:39:46 +0530

    ques

---
+++ MATHS/Ques/PowOfTwo.java
@@ -0,0 +1,11 @@
+public class PowOfTwo {
+    public static void main(String[] args) {
+        
+
+    int n=90;// note: fix for n=o;
+    
+    
+    boolean ans=(n&(n-1))==0;
+    System.out.println(ans);
+    
+}}
