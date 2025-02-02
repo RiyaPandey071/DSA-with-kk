35cc95833bce5611f588c8a3162fcd2df7e0ca8e
Riya Pandey <riyapandey0553@gmail.com>
2025-02-02 10:44:59 +0530

    ques

--- MATHS/MATH-2/SQRT.java
+++ MATHS/MATH-2/SQRT.java
@@ -1,6 +1,6 @@
 public class SQRT {
    public static void main(String[] args) {
-    int n=23;
+    int n=23 ;
     int p=3;
     System.out.printf("%.3f",sqrt(n,p));
   //  System.out.printf("%.2f",Math.sqrt(n));
@@ -10,19 +10,22 @@ int s=0;
 int e=n;
 double root=0.0;
 
-//while(s<=e){
-   // int m=s+(e-s)/2;
-    //if(m*m==n){
-        //return m;
 
-   // }
+while(s<=e){
+    int m=s+(e-s)/2;
+    if(m*m==n){
+        return m;
+}
+    
 
-    //if(m*m>n){
-       // e=m-1;
-   //}else{
-       // s=m+1;
-   // }
-//}
+    if(m*m>n){
+        e=m-1;
+      // root=e;
+   }else{
+        s=m+1;
+       root=m;
+    }
+}
 double inc=0.1;
 for(int i=0;i<p;i++){
 while(root*root<=n){
