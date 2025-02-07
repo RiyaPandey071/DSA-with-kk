449790de4ad250a471521bdd88d60eefc6b951b6
Riya Pandey <riyapandey0553@gmail.com>
2025-02-07 07:34:39 +0530

    Nto1

---
+++ RECURSION/QUES/Nto1.java
@@ -0,0 +1,29 @@
+
+public class Nto1 {
+    public static void main(String[] args) {
+     funboth(5);   
+    }
+    static void fun(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+" ");
+        fun(n-1);
+    }
+    static void funrev(int n){
+        if(n==0){
+            return;
+        }
+        
+        funrev(n-1);  //reverse
+        System.out.print(n+" "); 
+    }
+    static void funboth(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+" ");
+        funboth(n-1);  //reverse
+        System.out.print(n+" "); 
+    }
+}
