9b1405836fd107ad84a3a887ce7f721467f2bee8
Riya Pandey <riyapandey0553@gmail.com>
2025-03-28 10:14:39 +0530

    ques

--- RECURSION/QUES/SUBSET/SubSequence.java
+++ RECURSION/QUES/SUBSET/SubSequence.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 public class SubSequence {
   public static void main(String[] args) {
     subseq("","abc");
+   //2approach 
     ArrayList<String> sub = new ArrayList<>();
     subseq("", "abc", sub);
     System.out.println(sub);
+
+    //3rd 
+   System.out.println(subseqRet("","abc"));
   }
   
   static void subseq(String p, String up){
@@ -36,6 +40,20 @@ public class SubSequence {
   
   }
 
+  static ArrayList<String> subseqRet(String p, String up){
+    if(up.isEmpty()){
+       ArrayList<String> list= new ArrayList<>();
+       list.add(p);
+       return list;
+    }
+
+    char ch=up.charAt(0);
+
+    ArrayList<String> left= subseqRet(p+ch,up.substring(1));
+    ArrayList<String> right=subseqRet(p,up.substring(1));
+   left.addAll(right);
+   return left;
+  }
 
 
 }
