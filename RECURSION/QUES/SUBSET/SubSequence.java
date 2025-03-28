70cc133b36544dad4e0733a3586a5ee05c9fbae4
Riya Pandey <riyapandey0553@gmail.com>
2025-03-28 10:44:07 +0530

    ques

--- RECURSION/QUES/SUBSET/SubSequence.java
+++ RECURSION/QUES/SUBSET/SubSequence.java
@@ -10,6 +10,12 @@ public class SubSequence {
 
     //3rd 
    System.out.println(subseqRet("","abc"));
+
+   //4
+   subseqAscii("","abc");
+
+   //5
+   System.out.println(subseqAsciiRet("","abc"));
   }
   
   static void subseq(String p, String up){
@@ -53,6 +59,38 @@ public class SubSequence {
     ArrayList<String> right=subseqRet(p,up.substring(1));
    left.addAll(right);
    return left;
+  }
+
+
+  static void subseqAscii(String p, String up){
+    if(up.isEmpty()){
+        System.out.println(p);
+        return ;
+    }
+
+    char ch=up.charAt(0);
+
+    subseqAscii(p+ch,up.substring(1));
+    subseqAscii(p,up.substring(1));
+    subseqAscii(p+(ch+0),up.substring(1));
+   
+  }
+
+  static ArrayList<String> subseqAsciiRet(String p, String up){
+    if(up.isEmpty()){
+       ArrayList<String> list= new ArrayList<>();
+       list.add(p);
+       return list;
+    }
+
+    char ch=up.charAt(0);
+
+    ArrayList<String> first= subseqAsciiRet(p+ch,up.substring(1));
+    ArrayList<String> second=subseqRet(p,up.substring(1));
+    ArrayList<String> third= subseqRet(p+(ch+0),up.substring(1));
+   first.addAll(second);
+   first.addAll(third);
+   return first;
   }
 
 
---
+++ RECURSION/QUES/SUBSET/Subset.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subset {
+  public static void main(String[] args) {
+    
+  }
+  
+  static List<List<Integer>> subset(int[] arr){
+
+    List<List<Integer>> outer=new ArrayList<>();
+  }
+  
+
+
+}
